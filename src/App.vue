d5ce3e18b6a22d4fe9d803c2c38983394f1067e0
owner <alswns6327@naver.com>
2023-11-16 16:34:45 +0900

    is component

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,39 @@
 <template>
   <!-- <SolotTest></SolotTest> -->
-    <ScopedSlot></ScopedSlot>
+    <!-- <ScopedSlot></ScopedSlot> -->
+    <a v-for="tab in tabs" :key="tab.id" :style="{color : (tab.id === currentTab ? 'blue' : 'red')}"
+      @click="changeTab(tab.id)"
+    >{{ tab.label }}</a>
+    <br/>
+    <component :is="currentTab"></component>
+    
 </template>
 
 <script>
+import Coral from './components/Coral.vue';
+import Midway from './components/Midway.vue';
+import LeyteGulf from './components/LeyteGulf.vue';
 import ScopedSlot from './components/ScopedSlot.vue';
 import SolotTest from './components/SolotTest.vue';
 
   export default{
     name: "App",
-    components: { SolotTest, ScopedSlot }
+    components: { SolotTest, ScopedSlot, Coral, Midway, LeyteGulf },
+    data(){
+      return {
+        currentTab : 'Coral',
+        tabs : [
+          {id : "Coral", label : "Coral"},
+          {id : "Midway", label : "Midway"},
+          {id : "LeyteGulf", label : "LeyteGulf"},
+        ]
+      }
+    },
+    methods : {
+      changeTab(id){
+        this.currentTab = id;
+      }
+    }
 }
   
 </script>
